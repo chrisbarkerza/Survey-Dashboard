b65482734d92c6f8d1aea050668446d8f5fa9e41
chrisbarkerza <chris.barker@futureforesight.com>
2025-07-22 15:52:59 +0200

    all done

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+# #!/bin/bash
+# lsof -ti:8000 | xargs kill -9
+# python3 -m http.server 8000 &
+# SERVER_PID=$!
 
-python3 -m http.server 8000 &
-SERVER_PID=$!
-
-sleep 2
+# sleep 2
 
 open http://localhost:8000/index.html
 
-wait $SERVER_PID
+# wait $SERVER_PID
